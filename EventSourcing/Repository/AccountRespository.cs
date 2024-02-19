1382022fb095c24478555c626d56bff237b436bc
nourelnaby4 <nourelnaby4@gmail.com>
2024-02-19 22:06:24 +0200

    add Repository

---
+++ EventSourcing/Repository/AccountRespository.cs
@@ -0,0 +1,30 @@
+﻿using EventSourcing.Aggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Repository
+{
+    public class AccountRespository
+    {
+        public void Save(Account account)
+        {
+            var evts = account.UnCommittedEvents.ToList();
+            evts.Select(ev => new EventDescriptor(account.Id, 0, JsonSerializer.Serialize(ev), Environment.UserName, Guid.NewGuid().ToString());
+            //:TODO save in database
+            // _context.Events.AddRange(evts)
+            account.Commit();
+        }
+        public Account Load(Guid aggregateId)
+        {   //:TODO load from database
+            // _context.Events.Where(e=>e.AggragateId=aggregateId).OrderBy(x=>x.CreatedDate)
+            var account = new Account();
+            return account;
+        }
+
+        private record EventDescriptor(Guid AggregateId,long EventId,string EventData,string User,string CorrelationId);
+    }
+}
